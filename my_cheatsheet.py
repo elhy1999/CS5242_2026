8819f261dd1e95370f3f92f3fea36d37e0b1e705
E. Liu <ernestliu64@gmail.com>
2026-03-01 15:33:16 +0800

    Complete all exercises and add cheatsheet

---
+++ my_cheatsheet.py
@@ -0,0 +1,235 @@
+import time
+import torch
+import torch.nn as nn
+
+################## Initialization ##################
+my_tensor = torch.Tensor([1, 1]) # float tensor
+my_long_tensor = my_tensor.long() # LongTensor
+input_dim = 2; output_dim = 3; linear_layer = nn.Linear(input_dim, output_dim, bias=True) # Matrix of linear_layer is (output_dim x input_dim)
+softmax_layer = nn.Softmax(dim=0)
+torch.softmax(my_tensor, dim=0)
+
+class OneLayerNet(nn.Module):
+
+    def __init__(self, input_size, output_size):
+        super(OneLayerNet , self).__init__()
+        self.linear_layer = nn.Linear( input_size, output_size , bias=False)
+        
+    def forward(self, x):
+        y = self.linear_layer(x)
+        prob = torch.softmax(y, dim=1)
+        return prob
+
+class ThreeLayerNet(nn.Module):
+
+    def __init__(self, input_size, hidden_size1, hidden_size2,  output_size):
+        super(ThreeLayerNet , self).__init__()
+        
+        self.layer1 = nn.Linear(input_size, hidden_size1, bias=False)
+        self.layer2 = nn.Linear(hidden_size1, hidden_size2, bias=False)
+        self.layer3 = nn.Linear(hidden_size2, output_size, bias=False)
+        
+    def forward(self, x):
+        
+        y       = self.layer1(x)
+        y_hat   = torch.relu(y)
+        z       = self.layer2(y_hat)
+        z_hat   = torch.relu(z)
+        scores  = self.layer3(z_hat)
+        
+        return scores
+
+net = OneLayerNet(...)
+
+######### To check weight and bias values #########
+linear_layer.weight
+linear_layer.bias
+
+######### Setting weights and bias values #########
+with torch.no_grad():
+    linear_layer.weight[0, 0] = 1
+    linear_layer.weight[0, 1] = 2
+    ...
+
+
+############### Cross Entropy Loss ################
+criterion = nn.CrossEntropyLoss()
+labels = torch.LongTensor([0, 1, 1])
+scores = torch.Tensor([
+        [1, -1],
+        [-1, 1],
+        [-1, 1],
+    ]
+)
+criterion(scores, labels)
+
+################## Finalized Training loop ##################
+N = 60000; n_dim = 3; w = h = 28; train_data = test_data = torch.rand([N, n_dim, w, h]); train_label = test_label = torch.rand([N, 1]).long()
+
+criterion = nn.NLLLoss() # Used when you already computed log-probs 
+criterion = nn.CrossEntropyLoss() # Used on raw logits
+optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
+batch_size = 200
+num_epochs = 5000
+for epoch in range(num_epochs):
+    running_loss = 0
+    running_error = 0
+    num_batches = 0
+    shuffled_indices = torch.randperm(N)
+    for count in range(0, N, batch_size):
+        # Set dL/dU, dL/dV, dL/dW to be filled with zeros
+        optimizer.zero_grad()
+        
+        # create a minibatch
+        # indices = torch.LongTensor(batch_size).random_(0, N)
+        indices = shuffled_indices[count: count + batch_size]
+        minibatch_data = train_data[indices]
+        minibatch_label = train_label[indices]
+        
+        # reshape the minibatch
+        inputs = minibatch_data.view(batch_size, 784)
+        
+        # tell Pytorch to start tracking all operations that will be done on "inputs"
+        inputs.requires_grad_()
+
+        # forward the minibatch through the net  
+        minibatch_scores = net(inputs) 
+        
+        # Compute the average of the losses of the data points in the minibatch
+        loss = criterion(minibatch_scores, minibatch_label) 
+        
+        # backward pass to compute dL/dU, dL/dV and dL/dW    
+        loss.backward()
+        
+        # do one step of stochastic gradient descent: U=U-lr(dL/dU), V=V-lr(dL/dU), ...
+        optimizer.step()
+
+        # compute and accumulate stats
+        running_loss += loss.detach().item()
+        error = utils.get_error(minibatch_scores.detach(), minibatch_label) # Definition pasted below
+        running_error += error.item()
+        num_batches += 1
+    
+    # compute stats for the full training set
+    total_loss = running_loss / num_batches
+    total_error = running_error / num_batches
+    print('epoch=', epoch, '\t loss=', total_loss, '\t error=', total_error*100, 'percent')
+
+################## Finalized Training loop (with learning rate strategy) ##################
+start = time.time()
+lr = 0.05 # initial learning rate
+for epoch in range(num_epochs):
+    # learning rate strategy : divide the learning rate by 1.5 every 10 epochs
+    if epoch % 10 == 0 and epoch > 10: 
+        lr /= 1.5
+    
+    # create a new optimizer at the beginning of each epoch: give the current learning rate.   
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+
+    # Tracking metrics
+    running_loss = 0
+    running_error = 0
+    num_batches = 0
+    shuffled_indices = torch.randperm(N)
+    for count in range(0, N, batch_size):
+        # Set dL/dU, dL/dV, dL/dW to be filled with zeros
+        optimizer.zero_grad()
+
+        # create a minibatch
+        # indices = torch.LongTensor(batch_size).random_(0, N)
+        indices = shuffled_indices[count: count + batch_size]
+        minibatch_data = train_data[indices]
+        minibatch_label = train_label[indices]
+
+        # reshape the minibatch
+        inputs = minibatch_data.view(batch_size, 784)
+
+        # tell Pytorch to start tracking all operations that will be done on "inputs"
+        inputs.requires_grad_()
+
+        # forward the minibatch through the net  
+        scores=net(inputs) 
+
+        # Compute the average of the losses of the data points in the minibatch
+        loss = criterion(scores, minibatch_label) 
+        
+        # backward pass to compute dL/dU, dL/dV and dL/dW    
+        loss.backward()
+
+        # do one step of stochastic gradient descent: U=U-lr(dL/dU), V=V-lr(dL/dU), ...
+        optimizer.step()
+        
+        # compute and accumulate stats
+        running_loss += loss.detach().item()
+        error = utils.get_error( scores.detach() , minibatch_label)
+        running_error += error.item()
+        num_batches+=1
+    
+    # once the epoch is finished we divide the "running quantities"
+    # by the number of batches
+    total_loss = running_loss / num_batches
+    total_error = running_error / num_batches
+    elapsed_time = time.time() - start
+    
+    # every 10 epoch we display the stats 
+    # and compute the error rate on the test set  
+    if epoch % 10 == 0:
+        print(' ')
+        print('epoch=',epoch, ' time=', elapsed_time,
+              ' loss=', total_loss , ' error=', total_error*100 ,'percent lr=', lr)
+        eval_on_test_set()
+               
+
+################## Evaluation on Test Set ##################
+running_error = 0
+num_batches = 0
+for i in range(0, 10000, batch_size):
+
+    # extract the minibatch
+    minibatch_data = test_data[i: i + batch_size]
+    minibatch_label = test_label[i: i + batch_size]
+
+    # reshape the minibatch
+    inputs = minibatch_data.view(batch_size, 784)
+
+    # feed it to the network
+    scores = net(inputs)
+
+    # compute the error made on this batch
+    error = utils.get_error(scores, minibatch_label)
+    
+    # add it to the running error
+    running_error += error.item()
+
+    num_batches+=1
+
+# compute error rate on the full test set
+total_error = running_error/num_batches
+print( 'error rate on test set =', total_error*100 ,'percent')
+
+
+################### Misc. ###################
+def get_error(scores, labels):
+    """
+    Returns the classification error. Not to be used for regression errors
+    """
+    bs = scores.size(0)
+    predicted_labels = scores.argmax(dim=1)
+    indicator = predicted_labels == labels
+    num_matches = indicator.sum()
+
+    return 1 - num_matches.float() / bs
+
+def eval_on_test_set():
+    running_error = 0
+    num_batches = 0
+    for i in range(0, N, batch_size):
+        minibatch_data = test_data[i: i + batch_size]
+        minibatch_label = test_label[i: i + batch_size]
+        inputs = minibatch_data.view(batch_size, 784)
+        scores = net(inputs)
+        error = utils.get_error(scores, minibatch_label)
+        running_error += error.item()
+        num_batches+=1
+    total_error = running_error/num_batches
+    print('test error =', total_error*100 ,'percent')
